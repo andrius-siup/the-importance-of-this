6262a08c06102e58009e7726cfd684e1b3d1ff07
andrius-siup <andrius.siupinys@gmail.com>
2021-03-02 14:02:52 +0000

    Added event handler to button, to call methods for change background color.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $("p").click(function() {
     $(this).slideToggle('slow');
 })
+
+$("button").mouseenter(function() {
+    $("button").removeClass('makeRed').addClass('makeBlue');
+})
+
+$("button").mouseleave(function() {
+    $("button").removeClass('makeBlue').addClass('makeRed');
+})
